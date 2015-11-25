5ef946d7dce88a930cb42709eb357b3fb9d266d6
Isaac Shapira <isaac.shapira@logrhythm.com>
2015-11-25 16:26:03 -0700

    even more FS bindings

--- src/FS/Streams/Raw.elm
+++ src/FS/Streams/Raw.elm
@@ -1,8 +1,4 @@
-module FS.Streams.Raw
-  ( createReadStream
-  , createReadStream'
-  , createWriteStream
-  , createWriteStream' ) where
+module FS.Streams.Raw where
 
 import Task exposing (Task)
 
@@ -10,26 +6,18 @@ import Streams.Types exposing (..)
 import FS.Types exposing (..)
 import Native.FS.Streams
 
-createReadStream_ : ReadOptionsRaw -> FilePath -> Task x Read
-createReadStream_ =
-  Native.FS.Streams.createReadStream
-
 createReadStream' : ReadOptions -> FilePath -> Task x Readable
 createReadStream' o =
-  createReadStream_ (marshallReadOptions o)
+  Native.FS.Streams.createReadStream (marshallReadOptions o)
   >> Task.map Readable
 
 createReadStream : FilePath -> Task x Readable
 createReadStream =
   createReadStream' defaultReadOptions
 
-createWriteStream_ : WriteOptionsRaw -> FilePath -> Task x Write
-createWriteStream_ =
-  Native.FS.Streams.createWriteStream
-
 createWriteStream' : WriteOptions -> FilePath -> Task x Writable
 createWriteStream' o =
-  createWriteStream_ (marshallWriteOptions o)
+  Native.FS.Streams.createWriteStream (marshallWriteOptions o)
   >> Task.map Writable
 
 createWriteStream : FilePath -> Task x Writable

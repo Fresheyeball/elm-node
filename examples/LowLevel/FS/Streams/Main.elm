d2fc425e2c3d443a1dbf21b1c0031b4031647767
Isaac Shapira <fresheyeball@gmail.com>
2015-12-01 20:00:03 -0700

    back in the saddle

--- examples/LowLevel/FS/Streams/Main.elm
+++ examples/LowLevel/FS/Streams/Main.elm
@@ -15,17 +15,17 @@ testFile = dirname ++ "/testfile"
 flow : Mailbox Buffer
 flow = mailbox BufferEmpty
 
--- port log : Signal (Task x ())
--- port log =
---   Signal.map (logBuffer Utf8) flow.signal
+port log : Signal (Task x ())
+port log =
+  Signal.map (logBuffer Utf8) flow.signal
 
 port read : Task FSError ()
 port read =
   writeFileString testFile "success"
-  -- `andThen` always (createReadStream testFile)
-  -- `andThen` onBuffer Data flow.address
+  `andThen` always (createReadStream testFile)
+  `andThen` onBuffer Data flow.address
 
--- port write : Task x ()
--- port write =
---   createWriteStream (testFile ++ "-clone")
---   `andThen` writeBuffer flow.signal
+port write : Task x ()
+port write =
+  createWriteStream (testFile ++ "-clone")
+  `andThen` writeBuffer flow.signal

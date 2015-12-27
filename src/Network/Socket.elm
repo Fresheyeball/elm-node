03f61c9a503b3e053b791628c737dfb33c091a7e
Isaac Shapira <fresheyeball@gmail.com>
2015-12-27 02:52:47 -0700

    s/    /  /g

--- src/Network/Socket.elm
+++ src/Network/Socket.elm
@@ -1,13 +1,8 @@
-module Network.Socket where
-
+module Network.Socket (..) where
 
 import Task exposing (Task)
-
-
-import Foreign.Pattern exposing (..)
+import Foreign.Pattern.Task.Get exposing (..)
 import Foreign.Marshall exposing (unsafeToString)
-
-
 import Emitter.Unsafe exposing (on0, on1)
 import Network.Types exposing (..)
 import Chunk exposing (Chunk)
@@ -18,45 +13,48 @@ import Chunk exposing (Chunk)
 
 
 onClose : Socket -> Task x () -> Task x (Task x ())
-onClose = on0 "close"
+onClose =
+    on0 "close"
 
 
 onConnect : Socket -> Task x () -> Task x (Task x ())
 onConnect =
-  on0 "connect"
+    on0 "connect"
 
 
 onData : Socket -> (Chunk -> Task x ()) -> Task x (Task x ())
 onData s f =
-  on1 "data" s (f << Chunk.marshall)
+    on1 "data" s (f << Chunk.marshall)
 
 
 onDrain : Socket -> Task x () -> Task x (Task x ())
 onDrain =
-  on0 "drain"
+    on0 "drain"
 
 
 onEnd : Socket -> Task x () -> Task x (Task x ())
 onEnd =
-  on0 "end"
+    on0 "end"
 
 
 onError : Socket -> (String -> Task x ()) -> Task x (Task x ())
 onError s f =
-  on1 "error" s (f << unsafeToString)
+    on1 "error" s (f << unsafeToString)
 
 
 onTimeout : Socket -> Task x () -> Task x (Task x ())
 onTimeout =
-  on0 "timeout"
+    on0 "timeout"
 
 
 address : Socket -> Task x SocketAddress
-address = get0 "address"
-  >> Task.map marshallSocketAddress
+address =
+    get0 "address"
+        >> Task.map marshallSocketAddress
+
+
 
 -- socket.bufferSize
-
 -- socket.bytesRead
 -- socket.bytesWritten
 -- socket.connect(options[, connectListener])
